708752959e3c076b1cec8be36927bdb52bd6fe42
Dan Khayutin <dan.khayutin@gmail.com>
2018-07-19 19:51:28 -0400

    added views directory and index file to display blog

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ var Blog = mongoose.model('Blog', blogSchema);
 
 //Restful Routes
 
+app.get('/', function(req, res) {
+  res.redirect('/blogs')
+})
+
+app.get('/blogs', function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if(err){
+      console.log('Error')
+    } else {
+      res.render('index', {blogs: blogs});
+
+    }
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
 });
